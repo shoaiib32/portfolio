b8470e364e9bdabbd60077385253640ea01627f9
SHOAIB SIDDIQUE <168743937+shoaiib32@users.noreply.github.com>
2024-09-16 11:39:51 +0530

    fixed email
    email address was wrong in anchor tag

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">siddiqueshoaib38@gmail.com</a>
+          <a href="mailto:siddiqueshoaib38@gmail.com">siddiqueshoaib38@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
